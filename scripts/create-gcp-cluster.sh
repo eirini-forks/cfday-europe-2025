fa770a2cc09b8531f61a0b7e3b098e5cf4ee74b6
Georgi Sabev <georgethebeatle@gmail.com>
2025-08-26 12:52:12 +0000

    Remove the BucketIAMMember
    * We configure the service to run with our own service account so that
      opening the bucket for everyone is not needed anymore
    * The bukcet has the `forceDelete` set to `true` so that it can be
      deleted even if it contains objects
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- scripts/create-gcp-cluster.sh
+++ scripts/create-gcp-cluster.sh
@@ -3,5 +3,5 @@
 gcloud container clusters create cfday \
   --location europe-west1-b \
   --node-locations europe-west1-b \
-  --machine-type=e2-custom-4-6144 \
+  --machine-type=e2-custom-6-6144 \
   --num-nodes 1
